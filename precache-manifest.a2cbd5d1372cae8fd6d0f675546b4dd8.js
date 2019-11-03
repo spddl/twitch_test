e78f32489ca99c1b1bdbb1cb3794dcbfc4b3a6bb
spddl <spddlhh@gmail.com>
2019-11-03 11:31:44 +0100

    .

---
+++ precache-manifest.a2cbd5d1372cae8fd6d0f675546b4dd8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a83ca16102b0668b78a975e7cd2db3db",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e81aca630dc08e070da4",
+    "url": "/static/css/main.42341871.chunk.css"
+  },
+  {
+    "revision": "3590450f2752c6eacd5a",
+    "url": "/static/js/2.268f476b.chunk.js"
+  },
+  {
+    "revision": "e81aca630dc08e070da4",
+    "url": "/static/js/main.7225cd58.chunk.js"
+  },
+  {
+    "revision": "3e7d100673efc5d8259b",
+    "url": "/static/js/runtime-main.1062be0f.js"
+  }
+]);
