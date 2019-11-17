647db58be3aff46fe299ce8ed63d359ffe04bafe
spddl <spddlhh@gmail.com>
2019-11-17 20:05:06 +0100

    .

---
+++ precache-manifest.c8ffe439ac6de9a1f76051030016cba0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f82a6d574709b724a5e9cef9679cc418",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d570994e97f45504aaee",
+    "url": "/static/css/main.42341871.chunk.css"
+  },
+  {
+    "revision": "56c59c4d12ca2a695d14",
+    "url": "/static/js/2.e866cfc0.chunk.js"
+  },
+  {
+    "revision": "d570994e97f45504aaee",
+    "url": "/static/js/main.cd0e0e29.chunk.js"
+  },
+  {
+    "revision": "e9ff697483409c47e114",
+    "url": "/static/js/runtime-main.d17e4adc.js"
+  },
+  {
+    "revision": "5f80eca1d805425ff67f53ef32fd69af",
+    "url": "/static/media/TwitchGlitchWhite.5f80eca1.svg"
+  }
+]);
